ffe3f3d859fbc4007968dfb01f0a33f21a16d184
David <david.van.steertegem@gmail.com>
2020-01-22 09:50:26 +0100

    Initial Commit

---
+++ TestRest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestRest
+//
+//  Created by ontlener on 22/01/2020.
+//  Copyright © 2020 EhB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
